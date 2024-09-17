fc1802cf633fc2b59ca8266b8702c7d9055c35db
recehbijak <alamnurikhlas@gmail.com>
2024-09-17 12:21:47 +0700

    Add queryselector for buttons Add values and classes to buttons Add eventlistner that stores button values to digit variables

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let digitFirst = '';
 let digitOperator = '';
 let digitSecond = ''; 
+let displayValue = digitFirst + digitOperator + digitSecond;;
 
 function operate() {
     const intDigitFirst = Number(digitFirst);
@@ -39,3 +40,45 @@ function calcDivide(x, y) {
     return x / y;
 }
 
+const buttonsNum = document.querySelectorAll(".number-buttons");
+const buttonsOperator = document.querySelectorAll(".operator-buttons");
+const display = document.querySelector(".calc-display");
+const placeholder = document.querySelector(".placeholder")
+
+buttonsNum.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if (digitFirst !== '' && digitOperator !== '') {
+            digitSecond += e.target.value;
+            console.log(digitSecond);
+            showDisplayValues();
+        } else {
+            digitFirst += e.target.value;
+            console.log(digitFirst);
+            showDisplayValues();
+        }
+    })
+})
+
+buttonsOperator.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        digitOperator = e.target.value;
+        console.log(digitOperator);
+        showDisplayValues();
+    })
+})
+
+function showDisplayValues() {
+    placeholder.textContent = displayValue;
+}
+
+function displayFirstDigits() {
+    placeholder.textContent = digitFirst;
+}
+
+function displayOperator() {
+    placeholder.append(`${digitOperator}`)
+}
+
+function displaySecondDigits() {
+    placeholder.append(`${digitSecond}`)
+}
